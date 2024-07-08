826301e9b6be2036d5998e1c319bf774c7ec0be0
Abderrahman Youabd <youabd50@gmail.com>
2024-07-08 08:50:50 +0200

    add: first component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstCompComponent
   ],
   imports: [
     BrowserModule,
