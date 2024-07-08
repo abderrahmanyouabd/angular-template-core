6e23138f0e9a074e32863594134ca1fe314483ba
Abderrahman Youabd <youabd50@gmail.com>
2024-07-08 13:52:55 +0200

    feat: card component added

---
+++ src/app/form-details/form-details.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-form-details',
+  templateUrl: './form-details.component.html',
+  styleUrl: './form-details.component.scss'
+})
+export class FormDetailsComponent {
+  @Input()
+  message: any = {};
+  @Input()
+  index: number = -1;
+  @Output()
+  delete: EventEmitter<number> = new EventEmitter<number>();
+
+  onDelete() {
+    this.delete.emit(this.index);
+  }
+}
