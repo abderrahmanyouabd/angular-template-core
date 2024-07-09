42ad0d6bd0c3ec7a960c7bc1eee21e0961e6d8fd
Abderrahman Youabd <youabd50@gmail.com>
2024-07-09 06:11:17 +0200

    feat: added how to read params and queryParams (snapshot)

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent{
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.param = this.activatedRoute.snapshot.params['username'];
+    this.QueryParam = this.activatedRoute.snapshot.queryParams['course'];
+  }
+
+  param: any;
+  QueryParam: any;
 
 }
