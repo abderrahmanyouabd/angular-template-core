bc7cb6cbabeb6b4c6ec6ead476515749f2cb80b6
Abderrahman Youabd <youabd50@gmail.com>
2024-07-09 05:46:36 +0200

    feat: routes configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { AppComponent } from './app.component';
 import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 import {FormsModule} from "@angular/forms";
 import { FormDetailsComponent } from './form-details/form-details.component';
+import {MyFirstService} from "./services/my-first.service";
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirstCompComponent,
-    FormDetailsComponent
+    FormDetailsComponent,
+    MenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
